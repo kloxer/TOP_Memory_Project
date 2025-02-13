3d231f1da970510b709acbf31b51ecd113e516e7
kloxer <102638907+kloxer@users.noreply.github.com>
2025-02-12 22:33:35 -0500

    Added a custom search for gyphs and a custom home page, seperate from game

--- src/memorygame.jsx
+++ src/memorygame.jsx
@@ -1,257 +1,8 @@
 import { useEffect, useState } from "react";
 import "./images.css";
-const countryListAlpha2 = {
-  AF: "Afghanistan",
-  AL: "Albania",
-  DZ: "Algeria",
-  AS: "American Samoa",
-  AD: "Andorra",
-  AO: "Angola",
-  AI: "Anguilla",
-  AQ: "Antarctica",
-  AG: "Antigua and Barbuda",
-  AR: "Argentina",
-  AM: "Armenia",
-  AW: "Aruba",
-  AU: "Australia",
-  AT: "Austria",
-  AZ: "Azerbaijan",
-  BS: "Bahamas (the)",
-  BH: "Bahrain",
-  BD: "Bangladesh",
-  BB: "Barbados",
-  BY: "Belarus",
-  BE: "Belgium",
-  BZ: "Belize",
-  BJ: "Benin",
-  BM: "Bermuda",
-  BT: "Bhutan",
-  BO: "Bolivia (Plurinational State of)",
-  BQ: "Bonaire, Sint Eustatius and Saba",
-  BA: "Bosnia and Herzegovina",
-  BW: "Botswana",
-  BV: "Bouvet Island",
-  BR: "Brazil",
-  IO: "British Indian Ocean Territory (the)",
-  BN: "Brunei Darussalam",
-  BG: "Bulgaria",
-  BF: "Burkina Faso",
-  BI: "Burundi",
-  CV: "Cabo Verde",
-  KH: "Cambodia",
-  CM: "Cameroon",
-  CA: "Canada",
-  KY: "Cayman Islands (the)",
-  CF: "Central African Republic (the)",
-  TD: "Chad",
-  CL: "Chile",
-  CN: "China",
-  CX: "Christmas Island",
-  CC: "Cocos (Keeling) Islands (the)",
-  CO: "Colombia",
-  KM: "Comoros (the)",
-  CD: "Congo (the Democratic Republic of the)",
-  CG: "Congo (the)",
-  CK: "Cook Islands (the)",
-  CR: "Costa Rica",
-  HR: "Croatia",
-  CU: "Cuba",
-  CW: "Curaçao",
-  CY: "Cyprus",
-  CZ: "Czechia",
-  CI: "Côte d'Ivoire",
-  DK: "Denmark",
-  DJ: "Djibouti",
-  DM: "Dominica",
-  DO: "Dominican Republic (the)",
-  EC: "Ecuador",
-  EG: "Egypt",
-  SV: "El Salvador",
-  GQ: "Equatorial Guinea",
-  ER: "Eritrea",
-  EE: "Estonia",
-  SZ: "Eswatini",
-  ET: "Ethiopia",
-  FK: "Falkland Islands (the) [Malvinas]",
-  FO: "Faroe Islands (the)",
-  FJ: "Fiji",
-  FI: "Finland",
-  FR: "France",
-  GF: "French Guiana",
-  PF: "French Polynesia",
-  TF: "French Southern Territories (the)",
-  GA: "Gabon",
-  GM: "Gambia (the)",
-  GE: "Georgia",
-  DE: "Germany",
-  GH: "Ghana",
-  GI: "Gibraltar",
-  GR: "Greece",
-  GL: "Greenland",
-  GD: "Grenada",
-  GP: "Guadeloupe",
-  GU: "Guam",
-  GT: "Guatemala",
-  GG: "Guernsey",
-  GN: "Guinea",
-  GW: "Guinea-Bissau",
-  GY: "Guyana",
-  HT: "Haiti",
-  HM: "Heard Island and McDonald Islands",
-  VA: "Holy See (the)",
-  HN: "Honduras",
-  HK: "Hong Kong",
-  HU: "Hungary",
-  IS: "Iceland",
-  IN: "India",
-  ID: "Indonesia",
-  IR: "Iran (Islamic Republic of)",
-  IQ: "Iraq",
-  IE: "Ireland",
-  IM: "Isle of Man",
-  IL: "Israel",
-  IT: "Italy",
-  JM: "Jamaica",
-  JP: "Japan",
-  JE: "Jersey",
-  JO: "Jordan",
-  KZ: "Kazakhstan",
-  KE: "Kenya",
-  KI: "Kiribati",
-  KP: "Korea (the Democratic People's Republic of)",
-  KR: "Korea (the Republic of)",
-  KW: "Kuwait",
-  KG: "Kyrgyzstan",
-  LA: "Lao People's Democratic Republic (the)",
-  LV: "Latvia",
-  LB: "Lebanon",
-  LS: "Lesotho",
-  LR: "Liberia",
-  LY: "Libya",
-  LI: "Liechtenstein",
-  LT: "Lithuania",
-  LU: "Luxembourg",
-  MO: "Macao",
-  MG: "Madagascar",
-  MW: "Malawi",
-  MY: "Malaysia",
-  MV: "Maldives",
-  ML: "Mali",
-  MT: "Malta",
-  MH: "Marshall Islands (the)",
-  MQ: "Martinique",
-  MR: "Mauritania",
-  MU: "Mauritius",
-  YT: "Mayotte",
-  MX: "Mexico",
-  FM: "Micronesia (Federated States of)",
-  MD: "Moldova (the Republic of)",
-  MC: "Monaco",
-  MN: "Mongolia",
-  ME: "Montenegro",
-  MS: "Montserrat",
-  MA: "Morocco",
-  MZ: "Mozambique",
-  MM: "Myanmar",
-  NA: "Namibia",
-  NR: "Nauru",
-  NP: "Nepal",
-  NL: "Netherlands (the)",
-  NC: "New Caledonia",
-  NZ: "New Zealand",
-  NI: "Nicaragua",
-  NE: "Niger (the)",
-  NG: "Nigeria",
-  NU: "Niue",
-  NF: "Norfolk Island",
-  MP: "Northern Mariana Islands (the)",
-  NO: "Norway",
-  OM: "Oman",
-  PK: "Pakistan",
-  PW: "Palau",
-  PS: "Palestine, State of",
-  PA: "Panama",
-  PG: "Papua New Guinea",
-  PY: "Paraguay",
-  PE: "Peru",
-  PH: "Philippines (the)",
-  PN: "Pitcairn",
-  PL: "Poland",
-  PT: "Portugal",
-  PR: "Puerto Rico",
-  QA: "Qatar",
-  MK: "Republic of North Macedonia",
-  RO: "Romania",
-  RU: "Russian Federation (the)",
-  RW: "Rwanda",
-  RE: "Réunion",
-  BL: "Saint Barthélemy",
-  SH: "Saint Helena, Ascension and Tristan da Cunha",
-  KN: "Saint Kitts and Nevis",
-  LC: "Saint Lucia",
-  MF: "Saint Martin (French part)",
-  PM: "Saint Pierre and Miquelon",
-  VC: "Saint Vincent and the Grenadines",
-  WS: "Samoa",
-  SM: "San Marino",
-  ST: "Sao Tome and Principe",
-  SA: "Saudi Arabia",
-  SN: "Senegal",
-  RS: "Serbia",
-  SC: "Seychelles",
-  SL: "Sierra Leone",
-  SG: "Singapore",
-  SX: "Sint Maarten (Dutch part)",
-  SK: "Slovakia",
-  SI: "Slovenia",
-  SB: "Solomon Islands",
-  SO: "Somalia",
-  ZA: "South Africa",
-  GS: "South Georgia and the South Sandwich Islands",
-  SS: "South Sudan",
-  ES: "Spain",
-  LK: "Sri Lanka",
-  SD: "Sudan (the)",
-  SR: "Suriname",
-  SJ: "Svalbard and Jan Mayen",
-  SE: "Sweden",
-  CH: "Switzerland",
-  SY: "Syrian Arab Republic",
-  TW: "Taiwan",
-  TJ: "Tajikistan",
-  TZ: "Tanzania, United Republic of",
-  TH: "Thailand",
-  TL: "Timor-Leste",
-  TG: "Togo",
-  TK: "Tokelau",
-  TO: "Tonga",
-  TT: "Trinidad and Tobago",
-  TN: "Tunisia",
-  TR: "Turkey",
-  TM: "Turkmenistan",
-  TC: "Turks and Caicos Islands (the)",
-  TV: "Tuvalu",
-  UG: "Uganda",
-  UA: "Ukraine",
-  AE: "United Arab Emirates (the)",
-  GB: "United Kingdom of Great Britain and Northern Ireland (the)",
-  UM: "United States Minor Outlying Islands (the)",
-  US: "United States of America (the)",
-  UY: "Uruguay",
-  UZ: "Uzbekistan",
-  VU: "Vanuatu",
-  VE: "Venezuela (Bolivarian Republic of)",
-  VN: "Viet Nam",
-  VG: "Virgin Islands (British)",
-  VI: "Virgin Islands (U.S.)",
-  WF: "Wallis and Futuna",
-  EH: "Western Sahara",
-  YE: "Yemen",
-  ZM: "Zambia",
-  ZW: "Zimbabwe",
-  AX: "Åland Islands",
-};
 
+
+let highScore = 0;
 function DisplayGame() {
   let loading = false; //state can be calculated based on num of urls so no need to create a seperate state
 
@@ -266,11 +17,14 @@ function DisplayGame() {
   if (urls.length > 1) {
     loading = true;
   }
+  const [flag, setFlag] = useState(false);
+  const [previousImages, setPreviousImages] = useState([]);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
-    let flag = false;
+    let flags = false;
     async function getGif(query) {
-      const url = `https://api.giphy.com/v1/gifs/search?api_key=fcUtonT0Ion9FCqAtKat33zAmijwJ3JF&q=${query}&limit=12&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+      const url = `https://api.giphy.com/v1/gifs/search?api_key=zWj4OOUybyP8xoYmYOpivph7HplwFqTQ&q=${query}&limit=12&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
       const response = await fetch(url, { mode: "cors" });
       const json = await response.json();
       const arr = [];
@@ -282,21 +36,25 @@ function DisplayGame() {
         };
         arr.push(r);
       });
-      if (flag == true) {
+      if (flags == false) {
         setImg(arr);
       }
     }
-    getGif("cats");
+    getGif(query);
     return () => {
-      flag = true;
+      flags = true;
     };
-  }, []);
+  }, [flag]);
 
   function shuffle(e) {
     if (!previousImages.includes(e.target.id)) {
+
       setPreviousImages([...previousImages, e.target.id]);
       console.log("here");
     } else {
+      if (previousImages.length > highScore){
+        highScore = previousImages.length;
+      }
       setPreviousImages([]);
     }
 
@@ -318,34 +76,66 @@ function DisplayGame() {
     console.log("SETTING");
     setImg(array);
   }
-  const [previousImages, setPreviousImages] = useState([]);
+
+
+  function sendName(e){
+    console.log(e.target.value)
+    setQuery(e.target.value)
+  }
+  function startGame(){
+    setFlag(true);
+  }
 
   return (
     <div>
-      <h2>Cat Memory Game</h2>
-      <p className="score">Score: {previousImages.length}</p>
 
-      <div className="container">
-        {loading ? (
-          urls.map((url) => {
-            return (
-              <div
-                className="imgCard"
-                key={url.id}
-                id={url.id}
-                onClick={shuffle}
-              >
-                <img src={url.link} id={url.id} />
-                <p id={url.id}>{url.title}</p>
-              </div>
-            );
-          })
-        ) : (
-          <h2>"Loading..."</h2>
-        )}
-      </div>
+      {flag ? <ShowGame 
+      name={query}
+      loading={loading} 
+      urls={urls} 
+      previousImages={previousImages} 
+      highScore={highScore}
+      shuffle={shuffle} />
+      : <Start sendName={sendName} startGame={startGame}/> }
+
     </div>
   );
 }
+function Start({sendName, startGame}){
+return (
+  <div className="search">
+  <h2>Memory Game!</h2>
+  <p>Type the name of the gifs you would like to play with.</p>
+  <label htmlFor="search"></label>
+  <input onChange={sendName} type="text" />
+  <button onClick={startGame}>Sumbit</button>
+</div>
 
+)}
+function ShowGame({name, loading, urls, previousImages, highScore, shuffle}){
+return <>
+      <h2>{name} Memory Game</h2>
+      <p className="score">Score: {previousImages.length}</p>
+<p className="score">High Score: {highScore}</p>
+<div className="container">
+  {loading ? (
+    urls.map((url) => {
+      return (
+        <div
+          className="imgCard"
+          key={url.id}
+          id={url.id}
+          onClick={shuffle}
+        >
+          <img src={url.link} id={url.id} />
+          <p id={url.id}>{url.title}</p>
+        </div>
+      );
+    })
+  ) : (
+    <h2>"Loading..."</h2>
+  )}
+</div>
+</>
+}
 export default DisplayGame;
